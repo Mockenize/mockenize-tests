035f799961e2c97595ba53a5f14c3c6d858c0805
Mauricio Lima <mauriciol@ciandt.com>
2016-07-19 13:01:19 -0300

    fix file test

--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -1,4 +1,5 @@
 Given(/^a file with json$/) do |json|
+  Dir.mkdir '/tmp/mockenize' unless Dir.exists? '/tmp/mockenize'
   File.open("/tmp/mockenize/upload.json", "w+") do |file|
     file.write(json)
   end
